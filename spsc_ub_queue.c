17d90c6bb97a3eacae8eabeb1e7020013f988215
Steffen Vogel <post@steffenvogel.de>
2016-09-08 00:20:22 -0400

    added copyright

--- spsc_ub_queue.c
+++ spsc_ub_queue.c
@@ -1,3 +1,36 @@
+/** Lock-free Unbounded Single-Producer Single-consumer (SPSC) queue.
+ *
+ * Based on Dmitry Vyukov's Unbounded SPSC queue:
+ *   http://www.1024cores.net/home/lock-free-algorithms/queues/unbounded-spsc-queue
+ *
+ * @author Umar Farooq <umar1.farooq1@gmail.com>
+ * @copyright 2016 Steffen Vogel
+ * @license BSD 2-Clause License
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modiffication, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "spsc_ub_queue.h"
 
 void spsc_ub_queue_init(struct spsc_ub_queue* q, size_t size, const struct memtype *mem)
